3b56c6e97a60415a3831dddaeabe04dd3be3c5af
Leah <leahkang92@gmail.com>
2024-09-09 13:13:56 +0900

    0909

---
+++ script.js
@@ -0,0 +1,42 @@
+// 0909월 변수
+
+// let userName = '리아';
+// let _age = 30;
+// let $salary;
+// $salary = 5000;
+
+// _age = 45;
+
+// console.log(userName);
+// console.log(_age);
+// console.log($salary);
+
+// ----------------------------
+
+// var let 차이
+// var user_age = 50;
+// var user_age = 55;
+// console.log(user_age);
+
+// let new_user_age = 50;
+// let new_user_age = 55; //에러
+// console.log(new_user_age);
+
+// ----------------------------
+
+// 호이스팅 var let 차이
+// console.log(myVar);
+// var myVar = "hello";
+// // 출력결과 : undefined
+
+console.log(myVar);
+let myVar = "hello";
+// 출력결과 에러
+
+// ----------------------------
+
+let age = 10;
+let name = 'Leah';
+let isActive = true;
+let person = {name:'리아', age:31};  //객체
+let fruits = ['사과','바나나', '포도']; //배열
